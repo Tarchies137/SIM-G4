d4392da496e84a49a221fb696111f0615c2c943b
Tarchies137 <espinoza.fabian@gmail.com>
2023-11-16 10:24:55 -0300

    16/11/23 Actualizacion Home

--- sim/buil_HOME/CMakeFiles/sim.dir/DependInfo.cmake
+++ sim/buil_HOME/CMakeFiles/sim.dir/DependInfo.cmake
@@ -10,8 +10,10 @@ set(CMAKE_DEPENDS_LANGUAGES
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/action.cc" "CMakeFiles/sim.dir/action.cc.o" "gcc" "CMakeFiles/sim.dir/action.cc.o.d"
   "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/construction.cc" "CMakeFiles/sim.dir/construction.cc.o" "gcc" "CMakeFiles/sim.dir/construction.cc.o.d"
+  "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/detector.cc" "CMakeFiles/sim.dir/detector.cc.o" "gcc" "CMakeFiles/sim.dir/detector.cc.o.d"
   "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/generator.cc" "CMakeFiles/sim.dir/generator.cc.o" "gcc" "CMakeFiles/sim.dir/generator.cc.o.d"
   "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/physics.cc" "CMakeFiles/sim.dir/physics.cc.o" "gcc" "CMakeFiles/sim.dir/physics.cc.o.d"
+  "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/run.cc" "CMakeFiles/sim.dir/run.cc.o" "gcc" "CMakeFiles/sim.dir/run.cc.o.d"
   "/home/fabian/fabian/Simuladores/Geant4/SIM-G4/sim/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
   )
 
