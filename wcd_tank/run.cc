dea9636dfc573bbbfd1dcd6a82b5f13b78e22436
Tarchies137 <espinoza.fabian@gmail.com>
2023-11-29 14:59:34 -0300

    vaya no se como quedará esto

---
+++ wcd_tank/run.cc
@@ -0,0 +1,30 @@
+#include "run.hh"
+
+MyRunAction::MyRunAction()
+{}
+
+MyRunAction::~MyRunAction()
+{}
+
+void MyRunAction::BeginOfRunAction(const G4Run*)
+{
+	auto *man = G4AnalysisManager::Instance();
+
+	G4String filename = "output.root";
+	man->OpenFile(filename);
+
+	man->CreateNtuple("Hits", "Hits");
+	man->CreateNtupleIColumn("fEvent");
+	man->CreateNtupleDColumn("fX");
+	man->CreateNtupleDColumn("fY");
+	man->CreateNtupleDColumn("fZ");
+	man->FinishNtuple(0); 
+}
+
+void MyRunAction::EndOfRunAction(const G4Run*)
+{
+	auto *man = G4AnalysisManager::Instance();
+
+	man->Write();
+	man->CloseFile();
+} 
