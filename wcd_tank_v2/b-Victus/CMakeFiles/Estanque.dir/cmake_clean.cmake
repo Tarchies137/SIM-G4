acb1d2ff8f5aa551ccc759e4dbbc81b3ca08bd14
tarchies137 <espinoza.fabian@gmail.com>
2024-10-30 22:17:01 -0300

    VICTUS

--- wcd_tank_v2/b-Victus/CMakeFiles/Estanque.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Estanque"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Estanque.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
